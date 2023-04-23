fc580b8c887b07b42dd805586552ed52b129ef43
Saharsh Anand <saharshpro@hotmail.com>
2023-04-23 14:00:02 +0530

    Added styling in terminal texts

--- js/terminal.js
+++ js/terminal.js
@@ -27,14 +27,18 @@ class Terminal {
             this.options.setFiles({
                 "about.txt": [
                     "<h3>Hi 👋,</h3>",
+                    "&nbsp;",
                     "I am Saharsh, a software Developer from India.",
                     "Currently, I am working as an engineering Associate at Goldman Sachs.",
-                    "I love working on complex software problems and building simple solutions."
+                    "I love working on complex software problems and building simple solutions.",
+                    "I tend to write silly code in my free time. This portfolio being an example for instace.",
+                    "&nbsp;",
+                    "Feel free to hack around!"
                 ],
                 "socials.txt": [
-                    "LinkedIn - https://www.linkedin.com/in/5aharsh/",
-                    "Stackoverflow - https://stackoverflow.com/users/4720652",
-                    "GitHub - https://github.com/5aharsh"
+                    "LinkedIn - [ <a href='https://www.linkedin.com/in/5aharsh/'>https://www.linkedin.com/in/5aharsh/</a> ]",
+                    "Stackoverflow - [ <a href='https://stackoverflow.com/users/4720652'>https://stackoverflow.com/users/4720652</a> ]",
+                    "GitHub - [ <a href='https://github.com/5aharsh'>https://github.com/5aharsh</a> ]"
                 ],
                 "sample.txt": [
                     ""
@@ -87,15 +91,21 @@ class Terminal {
 
     execute(command) {
         this.appendLine("<div id='terminal-pointer'>[ <span>Saharsh@Desktop (@5aharsh) ~</span> ]</div>");
-        this.appendLine(command);
+        this.appendLine(command, "t-thin");
         this.processCommand(command);
         this.input.value = "";
     }
 
     appendLine(text) {
+        this.appendLine(text, null);
+    }
+
+    appendLine(text, modifierClass) {
         var terminalPrint = document.querySelector("#terminal-print");
         var terminalLine = document.createElement("span");
         terminalLine.className = "terminal-line";
+        if (modifierClass!=null)
+            terminalLine.className += " "+modifierClass;
         terminalLine.innerHTML = text;
         terminalPrint.appendChild(terminalLine);
         window.scrollTo(0, document.body.scrollHeight);
