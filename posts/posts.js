8f70eb1acc2c116e920bbaa8459de70e9b71de23
Saharsh <5aharsh@github.com>
2024-12-28 03:08:42 +0530

    Don't mind this change

--- posts/posts.js
+++ posts/posts.js
@@ -4,13 +4,13 @@ posts = [
         date: "28 Dec, 2024",
         tags: ["design", "development"],
         content: `
-        <p>So there was a small project I did back in school when I was learning Javascript, conveniently named - <a href="https://github.com/5aharsh/blog-json">blog-json</a>. It's a silly name, but it captures the essence of it. It uses a bit of vanilla JS magic to dynamically build a list of blog posts by reading a simple json variable in another JS file. Mind you, this was done back when there was no React or Angular (well there was AngularJS, but thats whole another thing from current Angular at this point). Anyway, because of the same reason I've been a bit too proud of it</p>
+        <p>So there was a small project I did back in school when I was learning Javascript, conveniently named - <a href="https://github.com/5aharsh/blog-json">blog-json</a>. It's a silly name, but it captures the essence of it. It uses a bit of vanilla JS magic to dynamically build a list of blog posts by reading a simple json variable in another JS file. Mind you, this was done back when there was no React or Angular (well there was AngularJS, but thats whole another thing from current Angular at this point). Anyway, because of the same reason I've been a bit too proud of it.</p>
 
-        <p>So anyway, on this random winter day I had this sudden urge to redo the whole thing and add it my portfolio as a blog-like section. If you look at the original project, there are some feature missing, search being the most important one I would say. But let's face it, who is ever going to search my posts anyway? On that note, I do have a reputation of leaving projects unfinished. So, let's just hope I add more posts in this section now and then.</p>
+        <p>For some reason, on this random day of winter, I had this sudden urge to redo the whole thing and add it my portfolio as a blog-like section. If you look at the original project, there are some feature missing, search being the most important one I would say. But let's face it, who is ever going to search my posts anyway? On that note, I do have a reputation of leaving projects unfinished. So, let's just hope I add more posts in this section now and then.</p>
 
-        <p>For the sake of sharing something while I'm writing this, here is a <a href="https://open.spotify.com/playlist/6J0Yvewusziq50oP2Ic77m">spotify playlist</a> I've been listening quite a lot lately. Neither too distracting nor too boring. Thinking about it, it's almost time for Spotify Wrapped thing to come out as it's end of the year. I think I'm getting distracted.</p>
+        <p>For the sake of sharing something while I'm writing this, here is a <a href="https://open.spotify.com/playlist/6J0Yvewusziq50oP2Ic77m">spotify playlist</a> I've been listening quite a lot lately. Neither too distracting nor too boring. Thinking about it, as its almost end of the year, it's almost time for Spotify Wrapped thing to come out. What an year has it been. I think I've finally started to get distracted.</p>
         
-        <p>The irony.</p>
+        <p>The irony. Cheers!</p>
         `
     }
 ]
